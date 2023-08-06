f9e8e32969bc0a324c611b50c8674a52e750a894
dwi <wdwihartarto@gmail.com>
2023-08-06 19:41:28 +0700

    modifiers

--- app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,37 +21,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxSize()
-//                    .background(Color.Green),
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.SpaceBetween
-//            ) {
-//                Text(text = "Hello")
-//                Text(text = "World")
-//                Text(text = "Hello")
-//            }
-
-            /**
-             * when in column use horizontalAlignment and verticalArrangement
-             * but in the row horizontalAlignment to horizontalArrangement and verticalArrangement to verticalAlignment
-             * so basically they swapped
-             **/
-
-            Row(
+            Column(
                 modifier = Modifier
-//                    .fillMaxSize(0.5f)
-                    .width(300.dp)
-//                    .height(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .requiredWidth(600.dp)
+
             ) {
-                Text(text = "Hello")
+                Text(text = "Hello", modifier = Modifier.clickable {
+                    
+                })
+                Spacer(modifier = Modifier.height(50.dp))
                 Text(text = "World")
-                Text(text = "Hello")
             }
         }
     }
