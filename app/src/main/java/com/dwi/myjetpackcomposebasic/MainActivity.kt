e8f9306010e3800e029f84dff8f2358bd27f25ef
dwi <wdwihartarto@gmail.com>
2023-08-06 19:19:32 +0700

    row, column, basic sizing

--- app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
@@ -3,44 +3,53 @@ package com.dwi.myjetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dwi.myjetpackcomposebasic.ui.theme.MyJetpackComposeBasicTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyJetpackComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(Color.Green),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                Text(text = "Hello")
+//                Text(text = "World")
+//                Text(text = "Hello")
+//            }
+
+            /**
+             * when in column use horizontalAlignment and verticalArrangement
+             * but in the row horizontalAlignment to horizontalArrangement and verticalArrangement to verticalAlignment
+             * so basically they swapped
+             **/
+
+            Row(
+                modifier = Modifier
+//                    .fillMaxSize(0.5f)
+                    .width(300.dp)
+//                    .height(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyJetpackComposeBasicTheme {
-        Greeting("Android")
     }
 }
