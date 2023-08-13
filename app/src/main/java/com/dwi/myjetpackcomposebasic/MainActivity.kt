354d249dde19fbe34e0edf7c02b7440caa49c610
dwi <wdwihartarto@gmail.com>
2023-08-13 12:31:26 +0700

    maka a progress circular with compose

--- app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
@@ -11,16 +11,21 @@ import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,60 +33,75 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember {
-                mutableStateOf(200.dp)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CircularProgressBar(percentage = 0.8f, number = 100)
             }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-//                tween(
-//                    durationMillis = 3000,
-//                    delayMillis = 300,
-//                    easing = LinearOutSlowInEasing
-//                ),
-//                spring(
-//                    Spring.DampingRatioHighBouncy
-//                ),
-//                keyframes {
-//                    durationMillis = 5000
-//                    sizeState at 0 with LinearEasing
-//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-//                    sizeState * 2f at 5000
-//                },
-                tween(
-                    durationMillis = 1000
-                ),
-                label = "size animation"
-            )
-            val infiniteTransition = rememberInfiniteTransition(label = "")
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(
-                        durationMillis = 2000
-                    ),
-                    repeatMode = RepeatMode.Reverse,
-                ), label = "infinite size animation"
-            )
-            Box(modifier = Modifier
-                .size(size)
-                .background(color),
-                contentAlignment = Alignment.Center) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }) {
-                    Text(
-                        "Increase Size"
-                    )
-                }
-            }
-
         }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0,
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay,
+        ),
+        label = ""
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)){
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold,
+        )
     }
 }
