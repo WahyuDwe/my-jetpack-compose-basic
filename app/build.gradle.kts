6bbc9388220354d64999b06649df0f458ccce8e3
dwi <wdwihartarto@gmail.com>
2023-08-13 13:10:44 +0700

    try navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha12")
+
+    implementation("androidx.navigation:navigation-compose:2.7.0")
 }
--- app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/dwi/myjetpackcomposebasic/MainActivity.kt
@@ -46,62 +46,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                CircularProgressBar(percentage = 0.8f, number = 100)
-            }
+            Navigation()
         }
-    }
-}
-
-@Composable
-fun CircularProgressBar(
-    percentage: Float,
-    number: Int,
-    fontSize: TextUnit = 28.sp,
-    radius: Dp = 50.dp,
-    color: Color = Color.Green,
-    strokeWidth: Dp = 8.dp,
-    animDuration: Int = 1000,
-    animDelay: Int = 0,
-) {
-    var animationPlayed by remember {
-        mutableStateOf(false)
-    }
-    val curPercentage = animateFloatAsState(
-        targetValue = if (animationPlayed) percentage else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay,
-        ),
-        label = ""
-    )
-    LaunchedEffect(key1 = true) {
-        animationPlayed = true
-
-    }
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.size(radius * 2f)
-    ) {
-        Canvas(modifier = Modifier.size(radius * 2f)){
-            drawArc(
-                color = color,
-                -90f,
-                360 * curPercentage.value,
-                useCenter = false,
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-        }
-
-        Text(
-            text = (curPercentage.value * number).toInt().toString(),
-            color = Color.Black,
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold,
-        )
     }
 }
